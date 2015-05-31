e2eda78cfd86bdbbba1e35822d25773849ca23a9
steve henderson <steven.j.henderson@gmail.com>
2015-05-30 23:01:42 -0400

    lpthw #9!!!

---
+++ lpthw9.py
@@ -0,0 +1,27 @@
+#lpthw9
+#Here's some new strage stuff, remember type it exactly
+
+days = "Mon Tues Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: " , days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+my_array = ["apples", "pears", "bananas"]
+
+print "First element" , my_array[0]
+
+
+print "Second element" , my_array[1]
+
+print "Third element" , my_array[2]
+
+
+

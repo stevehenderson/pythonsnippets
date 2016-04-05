5a61ae526dd069685d88f24d0f6c93938f614918
Steve Henderson <steven.j.henderson@gmail.com>
2016-04-05 00:29:52 -0400

    Lesson 13..

---
+++ lpthw13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called: " , script
+print "The first argument was " , first
+print "the second argument was ", second
+print "the third argument was ", third
+

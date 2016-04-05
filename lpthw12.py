d7a33c0f00ecfecfb44c608d34a80a33229207a8
Steve Henderson <steven.j.henderson@gmail.com>
2016-04-04 21:11:12 -0400

    Lesson 12..and I'm back!

---
+++ lpthw12.py
@@ -0,0 +1,10 @@
+age=raw_input("How old are you bro? ")
+height=raw_input("How tall are you? ")
+weight=raw_input("How much do you weight? ")
+
+#Debug formatter
+print "So you are %r and are %r high and weigh %r!" % (age,height,weight)
+
+
+#Print ready formatter
+print "So you are %s and are %s high and weigh %s!" % (age,height,weight)
